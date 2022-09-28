7462f11d93650f15bf58002f205598e7ac693fb8
yaseenm8161 <114585805+yaseenm8161@users.noreply.github.com>
2022-09-28 08:39:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "yaseen"; // update the part BETWEEN the "quotes"
+let userName = "yaseenm8161"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
